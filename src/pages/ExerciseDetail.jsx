9192ddeb4ce233c64d74566ad5bddb13dd66449f
Femzy1900 <adedokunfemi14@gmail.com>
2024-04-12 17:19:36 -0700

    creating the exercise details

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import {exerciseOptions, fetchData} from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercise from '../components/SimilarExercise';
 
 const ExerciseDetail = () => {
   return (
-    <div>
-      ExerciseDetails
-    </div>
+    <Box>
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercise />
+
+    </Box>
   )
 }
 
